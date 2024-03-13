79fedfab7caa9f0e14d18d9280d65f00eeaec2e7
Daniel Ospina Ramirez <danielospinar@gmail.com>
2024-03-12 21:06:43 -0500

    Added Static Files

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const server = express();
 server.use(express.json());
 server.name = "API";
 
+server.use("/public", express.static(path.join(__dirname, "public")));
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
